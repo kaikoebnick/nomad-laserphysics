5d06ade8bd62790a17e799d9de16c2104e00a58a
kaikoebnick <kai.koebnick@gmail.com>
2024-10-27 22:06:54 +0100

    Update __init__.py

--- src/nomad_laserphysics/apps/__init__.py
+++ src/nomad_laserphysics/apps/__init__.py
@@ -1,5 +1,5 @@
 # ruff: noqa: E501
-#from nomad.config import _plugins
+from nomad.config import _plugins
 from nomad.config.models.plugins import AppEntryPoint
 from nomad.config.models.ui import (
     AlignEnum,
@@ -25,26 +25,26 @@ from nomad.config.models.ui import (
 )
 
 # Workaround: read the upload_ids from plugin's raw config.
-#try:
-#    upload_ids = _plugins['entry_points']['options']['nomad_laserphysics.apps:app_entry_point'][
-#        'upload_ids'
-#    ]
-#except KeyError:
-#    upload_ids = None
-#
-#if upload_ids:
-#    filters_locked = {
-#        'upload_id': upload_ids,
-#        'section_defs.definition_qualified_name': [
-#            'nomad_laserphysics.schema_packages.laserphysicsELN'
-#        ],
-#    }
-#else:
-#    filters_locked = {
-#        'section_defs.definition_qualified_name': [
-#            'nomad_laserphysics.schema_packages.laserphysicsELN'
-#        ]
-#    }
+try:
+    upload_ids = _plugins['entry_points']['options']['nomad_laserphysics.apps:app_entry_point'][
+        'upload_ids'
+    ]
+except KeyError:
+    upload_ids = None
+
+if upload_ids:
+    filters_locked = {
+        'upload_id': upload_ids,
+        'section_defs.definition_qualified_name': [
+            'nomad_laserphysics.schema_packages.laserphysicsELN'
+        ],
+    }
+else:
+    filters_locked = {
+        'section_defs.definition_qualified_name': [
+            'nomad_laserphysics.schema_packages.laserphysicsELN'
+        ]
+    }
 
 app_entry_point = AppEntryPoint(
     name='Laserphysics',
@@ -58,38 +58,38 @@ app_entry_point = AppEntryPoint(
             include=['*#nomad_laserphysics.schema_packages.laserphysicsELN'],
             exclude=['*#nomad.datamodel.metainfo.eln.BasicEln'],
         ),
-        #filters_locked=filters_locked,
+        filters_locked=filters_locked,
         
         columns=Columns(
             include=[
                 'entry_id',
                 'entry_type',
                 'authors',
-                'data.name#nomad_laserphysics.schema_packages.laserphysicsELN',
-                'data.category#nomad_laserphysics.schema_packages.laserphysicsELN',
-                #'data.platform#nomad_laserphysics.schema_packages.laserphysicsELN',
-                'data.date#nomad_laserphysics.schema_packages.laserphysicsELN',
+                'data.name#nomad_laserphysics.schema_packages.schema_package.laserphysicsELN',
+                'data.category#nomad_laserphysics.schema_packages.schema_package.laserphysicsELN',
+                #'data.platform#nomad_laserphysics.schema_packages.schema_package.laserphysicsELN',
+                'data.date#nomad_laserphysics.schema_packages.schema_package.laserphysicsELN',
             ],
             selected=[
-                'data.name#nomad_laserphysics.schema_packages.laserphysicsELN',
+                'data.name#nomad_laserphysics.schema_packages.schema_package.laserphysicsELN',
                 'authors',
-                'data.category#nomad_laserphysics.schema_packages.laserphysicsELN',
-                'data.date#nomad_laserphysics.schema_packages.laserphysicsELN',
+                'data.category#nomad_laserphysics.schema_packages.schema_package.laserphysicsELN',
+                'data.date#nomad_laserphysics.schema_packages.schema_package.laserphysicsELN',
             ],
             options={
                 'entry_id': Column(),
                 'entry_type': Column(label='Entry type', align=AlignEnum.LEFT),
                 'authors': Column(label='Authors', align=AlignEnum.LEFT),
-                'data.name#nomad_laserphysics.schema_packages.laserphysicsELN': Column(
+                'data.name#nomad_laserphysics.schema_packages.schema_package.laserphysicsELN': Column(
                     label='Name', align=AlignEnum.LEFT
                 ),
-                'data.category#nomad_laserphysics.schema_packages.laserphysicsELN': Column(
+                'data.category#nomad_laserphysics.schema_packages.schema_package.laserphysicsELN': Column(
                     label='Category'
                 ),
-                #'data.platform#nomad_laserphysics.schema_packages.laserphysicsELN': Column(
+                #'data.platform#nomad_laserphysics.schema_packages.schema_package.laserphysicsELN': Column(
                 #    label='Platform', align=AlignEnum.LEFT
                 #),
-                'data.date#nomad_laserphysics.schema_packages.laserphysicsELN': Column(
+                'data.date#nomad_laserphysics.schema_packages.schema_package.laserphysicsELN': Column(
                     label='Last update',
                     align=AlignEnum.LEFT,
                     format=Format(mode=ModeEnum.DATE),
@@ -109,7 +109,7 @@ app_entry_point = AppEntryPoint(
             widgets=[
                 WidgetTerms(
                     type='terms',
-                    quantity='data.category#nomad_laserphysics.schema_packages.laserphysicsELN',
+                    quantity='data.category#nomad_laserphysics.schema_packages.schema_package.laserphysicsELN',
                     scale=ScaleEnum.POW1,
                     layout={
                         BreakpointEnum.XXL: Layout(h=6, w=6, x=0, y=0),
@@ -121,7 +121,7 @@ app_entry_point = AppEntryPoint(
                 ),
                 #WidgetTerms(
                 #    type='terms',
-                #    quantity='data.methods.name#nomad_laserphysics.schema_packages.laserphysicsELN',
+                #    quantity='data.methods.name#nomad_laserphysics.schema_packages.schema_package.laserphysicsELN',
                 #    title='Methods',
                 #    scale=ScaleEnum.POW1,
                 #    layout={
@@ -134,7 +134,7 @@ app_entry_point = AppEntryPoint(
                 #),
                 #WidgetTerms(
                 #    type='terms',
-                #    quantity='data.systems.name#nomad_aitoolkit.schema.AIToolkitNotebook',
+                #    quantity='data.systems.name#nomad_aitoolkit.schema.schema_package.AIToolkitNotebook',
                 #    title='Systems',
                 #    scale=ScaleEnum.POW1,
                 #    layout={
