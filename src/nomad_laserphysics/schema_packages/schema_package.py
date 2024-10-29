4140d715b2419942a88e04bb8036eb374c869b0c
kaikoebnick <kai.koebnick@gmail.com>
2024-10-29 08:57:57 +0100

    Update schema_package.py

--- src/nomad_laserphysics/schema_packages/schema_package.py
+++ src/nomad_laserphysics/schema_packages/schema_package.py
@@ -1,94 +1,3 @@
-"""from typing import (
-    TYPE_CHECKING,
-)
-
-if TYPE_CHECKING:
-    from nomad.datamodel.datamodel import (
-        EntryArchive,
-    )
-    from structlog.stdlib import (
-        BoundLogger,
-    )
-
-from nomad.config import config
-from nomad.datamodel.data import (
-Datetime,
-Schema,
-)
-from nomad.datamodel.metainfo.annotations import ELNAnnotation, ELNComponentEnum
-from nomad.metainfo import MEnum, Quantity, SchemaPackage
-
-configuration = config.get_plugin_entry_point(
-    'custom_schema.schema_packages:schema_package_entry_point'
-)
-
-m_package = SchemaPackage()
-
-
-class NewSchemaPackage(Schema):
-    measurement_name = Quantity(
-        type=str,
-        description='''A nice and short name for your measurement.''',
-        a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity)
-    )
-    date_and_time = Quantity(
-        type=Datetime,
-        description='''Date and time the measurement was taken.''',
-        #default=
-        a_eln=ELNAnnotation(component=ELNComponentEnum.DateTimeEditQuantity)
-    )
-    material = Quantity(
-        type=str,
-        description='''Enter your material.''',
-        a_eln=ELNAnnotation(
-            component=ELNComponentEnum.EnumEditQuantity,
-            suggestions=['gold', 'silver'],
-        )
-    )
-    nice_peak = Quantity(
-        type=bool,
-        description='''Check if there is a nice peak in the measurement.''',
-        a_eln=ELNAnnotation(component=ELNComponentEnum.BoolEditQuantity) #default
-    )
-    measurement_category = Quantity(
-        type=MEnum('calibration', 'real_measurment'),
-        description='''Choose your category.''',
-        a_eln=ELNAnnotation(
-            component=ELNComponentEnum.RadioEnumEditQuantity,
-        )
-    )
-    voltage = Quantity(
-        type=float,
-        description='''Enter your voltage.''',
-        a_eln=ELNAnnotation(
-            component=ELNComponentEnum.NumberEditQuantity,
-        )
-    )
-    number_of_people_killed_during_measurement = Quantity(
-        type=int,
-        description='''Let us hope it is a zero.''',
-        a_eln=ELNAnnotation(
-            component=ELNComponentEnum.SliderEditQuantity,
-            minValue=0,
-            maxValue=50,
-        )
-    )
-    description = Quantity(
-        type=str,
-        description='''You can add text as well as pictures.''',
-        a_eln=ELNAnnotation(component=ELNComponentEnum.RichTextEditQuantity)
-    )
-    sample_id = Quantity(
-        type=str,
-        description='''The ID will be automatically generated.''',
-        a_eln=ELNAnnotation(default=f'{date_and_time}--{measurement_name}'),
-    )
-
-
-m_package.__init_metainfo__()"""
-
-
-
 from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -125,29 +34,24 @@ def remove_tags(text):
     return ''.join(xml.etree.ElementTree.fromstring(text).itertext())
 
 
-class Material(ArchiveSection):
+class Measurement(ArchiveSection):
     m_def = Section(a_eln=ELNAnnotation(overview=True))
 
-    name = Quantity(
+    material = Quantity(
         type=str,
         a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity),
         description='Name of the material.',
     )
-    formula = Quantity(
-        type=str,
-        a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity),
-        description='Chemical formula of the material.',
+    voltage = Quantity(
+        type=float,
+        description='Voltage.',
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.NumberEditQuantity,
+        )
+
+    references = SubSection(section=Reference, repeats=True)
+
     )
-
-
-"""class System(ArchiveSection):
-    m_def = Section(a_eln=ELNAnnotation(overview=True))
-
-    name = Quantity(
-        type=str,
-        a_eln=ELNAnnotation(component=ELNComponentEnum.StringEditQuantity),
-        description='Specifying name of the system.',
-    )"""
 
 
 class Author(ArchiveSection):
@@ -256,20 +160,13 @@ class laserphysicsELN(Schema):
         ),
     )
 
-    tag_1 = Quantity(
+    tag = Quantity(
         type=bool,
-        description='''Check if there is a nice peak in tag_1.''',
+        description='''Check if there is tag.''',
         a_eln=ELNAnnotation(component=ELNComponentEnum.BoolEditQuantity)
     )
 
-    voltage = Quantity(
-        type=float,
-        description='''Enter your voltage.''',
-        a_eln=ELNAnnotation(
-            component=ELNComponentEnum.NumberEditQuantity,
-        )
-    )
-    number_of_people_killed_during_measurement = Quantity(
+    """number_of_people_killed_during_measurement = Quantity(
         type=int,
         description='''Let us hope it is zero.''',
         a_eln=ELNAnnotation(
@@ -277,22 +174,12 @@ class laserphysicsELN(Schema):
             minValue=0,
             maxValue=50,
         )
-    )
-
-    """platform = Quantity(
-        type=MEnum(['Python', 'Julia', 'R', 'other']),
-        a_eln=ELNAnnotation(
-            component=ELNComponentEnum.AutocompleteEditQuantity,
-        ),
     )"""
+
 
     authors = SubSection(section=Author, repeats=True)
 
     material = SubSection(section=Material, repeats=True)
-
-#    systems = SubSection(section=System, repeats=True)
-
-    references = SubSection(section=Reference, repeats=True)
 
     def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
         super().normalize(archive, logger)
