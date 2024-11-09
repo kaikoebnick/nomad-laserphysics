e380ee22d3324c111628ca6bbafcbef61e7b418e
kaikoebnick <kai.koebnick@gmail.com>
2024-11-09 13:06:34 +0000

    Update schema_package.py

--- src/nomad_laserphysics/schema_packages/schema_package.py
+++ src/nomad_laserphysics/schema_packages/schema_package.py
@@ -8,7 +8,7 @@ if TYPE_CHECKING:
 import datetime
 import xml
 
-#from ase.data import chemical_symbols
+from ase.data import chemical_symbols
 from nomad.datamodel.data import (
     ArchiveSection,
     EntryDataCategory,
@@ -116,19 +116,7 @@ class Measurement(ArchiveSection):
 
 
     material = Quantity(
-        #type=MEnum(chemical_symbols),
-        type=MEnum( 'Ac', 'Ag', 'Al', 'Am', 'Ar', 'As', 'At', 'Au', 'B', 'Ba',
-    'Be', 'Bh', 'Bi', 'Bk', 'Br', 'C', 'Ca', 'Cd', 'Ce', 'Cf',
-    'Cl', 'Cm', 'Cn', 'Co', 'Cr', 'Cs', 'Cu', 'Db', 'Ds', 'Dy',
-    'Er', 'Es', 'Eu', 'F', 'Fe', 'Fl', 'Fm', 'Fr', 'Ga', 'Gd',
-    'Ge', 'H', 'He', 'Hf', 'Hg', 'Ho', 'Hs', 'I', 'In', 'Ir',
-    'K', 'Kr', 'La', 'Li', 'Lr', 'Lu', 'Lv', 'Mc', 'Md', 'Mg',
-    'Mn', 'Mo', 'Mt', 'N', 'Na', 'Nb', 'Nd', 'Ne', 'Nh', 'Ni',
-    'No', 'Np', 'O', 'Og', 'Os', 'P', 'Pa', 'Pb', 'Pd', 'Pm',
-    'Po', 'Pr', 'Pt', 'Pu', 'Ra', 'Rb', 'Re', 'Rf', 'Rg', 'Rh',
-    'Rn', 'Ru', 'S', 'Sb', 'Sc', 'Se', 'Sg', 'Si', 'Sm', 'Sn',
-    'Sr', 'Ta', 'Tb', 'Tc', 'Te', 'Th', 'Ti', 'Tl', 'Tm', 'Ts',
-    'U', 'V', 'W', 'X', 'Xe', 'Y', 'Yb', 'Zn', 'Zr'),
+        type=MEnum(chemical_symbols),
         shape= ['0..*'], #['n_atoms'],
         default=[],
         a_eln=ELNAnnotation(
