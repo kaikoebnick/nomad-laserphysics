a1f9ea7435d6270ea84e36da9e6ad1ed4f17aec2
kaikoebnick <kai.koebnick@gmail.com>
2024-11-12 11:04:30 +0000

    Update schema_package.py

--- src/nomad_laserphysics/schema_packages/schema_package.py
+++ src/nomad_laserphysics/schema_packages/schema_package.py
@@ -128,6 +128,31 @@ class Measurement(ArchiveSection):
             Elasticsearch(suggestion='simple'),
         ],
     )
+    material_1 = Quantity(
+        type=str,
+        a_eln=ELNAnnotation(
+            component=ELNComponentEnum.EnumEditQuantity,
+            props=dict(
+                suggestions=[
+                     'Ac', 'Ag', 'Al', 'Am', 'Ar', 'As', 'At', 'Au',
+    'B', 'Ba', 'Be', 'Bh', 'Bi', 'Bk', 'Br', 'C',
+    'Ca', 'Cd', 'Ce', 'Cf', 'Cl', 'Cm', 'Cn', 'Co',
+    'Cr', 'Cs', 'Cu', 'Db', 'Ds', 'Dy', 'Er', 'Es',
+    'Eu', 'F', 'Fe', 'Fl', 'Fm', 'Fr', 'Ga', 'Gd',
+    'Ge', 'H', 'He', 'Hf', 'Hg', 'Ho', 'Hs', 'I',
+    'In', 'Ir', 'K', 'Kr', 'La', 'Li', 'Lr', 'Lu',
+    'Lv', 'Mc', 'Md', 'Mg', 'Mn', 'Mo', 'Mt', 'N',
+    'Na', 'Nb', 'Nd', 'Ne', 'Nh', 'Ni', 'No', 'Np',
+    'O', 'Og', 'Os', 'P', 'Pa', 'Pb', 'Pd', 'Pm',
+    'Po', 'Pr', 'Pt', 'Pu', 'Ra', 'Rb', 'Re', 'Rf',
+    'Rg', 'Rh', 'Rn', 'Ru', 'S', 'Sb', 'Sc', 'Se',
+    'Sg', 'Si', 'Sm', 'Sn', 'Sr', 'Ta', 'Tb', 'Tc',
+    'Te', 'Th', 'Ti', 'Tl', 'Tm', 'Ts', 'U', 'V',
+    'W', 'X', 'Xe', 'Y', 'Yb', 'Zn', 'Zr'
+                ]
+            ),
+        ),
+    )
 
 
     voltage = Quantity(
