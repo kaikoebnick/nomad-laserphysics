01b62e75387ebed853568d6cd99215d23b3d0e09
Kai Köbnick <kai.koebnick@gmail.com>
2024-12-13 14:34:18 +0000

    Update schema

--- src/nomad_laserphysics/schema_packages/eln_schema.py
+++ src/nomad_laserphysics/schema_packages/eln_schema.py
@@ -181,7 +181,7 @@ class Measurement(ArchiveSection):
     m_def = Section(a_eln=ELNAnnotation(overview=True))
 
     tip = Quantity(
-        type=Reference(SectionProxy('tip_schema.laserphysicsTip.m_def')),
+        type=Reference(SectionProxy('tip_schema.laserphysicsTip')),
         description="""Type of the tip.""",
         a_eln=ELNAnnotation(
             component=ELNComponentEnum.ReferenceEditQuantity,
