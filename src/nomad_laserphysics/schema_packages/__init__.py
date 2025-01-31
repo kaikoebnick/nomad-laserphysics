6b71608672a868604b3b400e6ab311f13c1cec8a
Kai Koebnick <kai.koebnick@gmail.com>
2025-01-31 16:02:28 +0100

    update

--- src/nomad_laserphysics/schema_packages/__init__.py
+++ src/nomad_laserphysics/schema_packages/__init__.py
@@ -14,11 +14,14 @@ class FEMCorrelationChamberSchemaPackageEntryPoint(SchemaPackageEntryPoint):
     parameter: int = Field(0, description='Custom configuration parameter')
 
     def load(self):
-        from src.nomad_laserphysics.schema_packages.FEM_correlation_chamber_schema import m_package
+        from src.nomad_laserphysics.schema_packages.FEM_correlation_chamber_schema import (
+            m_package,
+        )
 
         return m_package
 
-FEM_correlation_chamber_schema_package_entry_point = FEMCorrelationChamberSchemaPackageEntryPoint(
+FEM_correlation_chamber_schema_package_entry_point = \
+        FEMCorrelationChamberSchemaPackageEntryPoint(
     name='FEM correlation chamber schema',
     description='New schema package entry point configuration.',
     plugin_package='schema_packages',
