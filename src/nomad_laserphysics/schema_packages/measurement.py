addf01597f03463b71206571f25509ef7a00b548
Kai Koebnick <kai.koebnick@gmail.com>
2025-02-03 12:45:54 +0100

    bug fix

--- src/nomad_laserphysics/schema_packages/measurement.py
+++ src/nomad_laserphysics/schema_packages/measurement.py
@@ -195,7 +195,7 @@ class Measurement(Schema):
 
     ions = Quantity(
         type=bool,
-        labe='ions',
+        label='ions',
         description="Check if there are ions.",
         a_eln=ELNAnnotation(component=ELNComponentEnum.BoolEditQuantity),
     )
@@ -267,7 +267,7 @@ class Measurement(Schema):
 
         #make tags searchable
         boolean_to_tag_map = {}
-        #logger.info(f"Zeug: {dir(self)}")
+        logger.info(f"Zeug: {dir(self)}")
         #for quant in dir(self):
                 #if quant.type is bool:
                     #boolean_to_tag_map[quant.label] = quant
