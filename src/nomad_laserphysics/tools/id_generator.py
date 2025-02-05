c90d2bc15db1be5e17aa3035d2ea969b85e5cd1f
Kai Koebnick <kai.koebnick@gmail.com>
2025-02-05 09:02:24 +0100

    new id mechanism

--- src/nomad_laserphysics/tools/id_generator.py
+++ src/nomad_laserphysics/tools/id_generator.py
@@ -1,18 +1,18 @@
 import hashlib
 
 # 62 possible values
-BASE62_ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+ALPHABET = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
 
-def encode(num, length=4):
+def encode(num, length=5):
     encoded = ""
     for _ in range(length):
         num, rem = divmod(num, 62)
-        encoded = BASE62_ALPHABET[rem] + encoded
+        encoded = ALPHABET[rem] + encoded
     return encoded
 
-def generate_id(name): # use 4 characters -> 16^4 values
-    hash_value = int(hashlib.sha1(name.encode()).hexdigest(), 16)
-    short_id = encode(hash_value % (62**4), 4)
+def generate_id(name): # use 5 characters -> 16^5 values
+    hash_value = int(hashlib.sha1(name.encode()).hexdigest(), 16) #160 Bit Hash
+    short_id = encode(hash_value % (62**5), 5)
     return short_id
 
 print(generate_id("04-02-24_testname"))
