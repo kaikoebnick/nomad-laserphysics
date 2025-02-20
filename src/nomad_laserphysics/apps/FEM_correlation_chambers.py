89cce8d00d29e22050f8cc71b65ff09ef2482e8e
Kai Koebnick <kai.koebnick@gmail.com>
2025-02-20 21:56:43 +0100

    fix

--- src/nomad_laserphysics/apps/FEM_correlation_chambers.py
+++ src/nomad_laserphysics/apps/FEM_correlation_chambers.py
@@ -87,10 +87,6 @@ FEMCorrelationChambers = App(
                     title='category',
                     search_quantity='data.category#nomad_laserphysics.schema_packages.FEM_correlation_chamber.FEMCorrelationChamber',
                     ),
-                    MenuItemTerms(
-                    title='category',
-                    search_quantity='data.category#nomad_laserphysics.schema_packages.FIM_test_chamber.FIMTestChamber',
-                    ),
                 ],
             ),
             Menu(
